65a50ca939e220128b5afa1a1a186038fb6165f4
Nimisha <nimisaraut@gmail.com>
2022-09-05 10:49:32 +0545

    main containerss

---
+++ src/Components/TypingBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TypingBox = () => {
+  return (
+    <div className='box-container'>
+        <div className='typing-container'>
+            
+        </div>
+        <div className='result-preview'>
+
+        </div>
+    </div>
+  )
+}
+
+export default TypingBox
