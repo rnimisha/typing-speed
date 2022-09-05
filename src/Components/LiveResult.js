9a68d71d8aa14b9b0b9bef19915be50a8eeda537
Nimisha <nimisaraut@gmail.com>
2022-09-05 10:58:46 +0545

    result preview style added

---
+++ src/Components/LiveResult.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LiveResult = () => {
+  return (
+    <div className='mini-result-box'>
+            20 sec
+    </div>
+  )
+}
+
+export default LiveResult
--- src/Components/TypingBox.js
+++ src/Components/TypingBox.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import LiveResult from './LiveResult'
 
 const TypingBox = () => {
   return (
     <div className='box-container'>
         <div className='typing-container'>
-            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure omnis ea, doloremque, ut accusantium blanditiis, porro pariatur id soluta harum cupiditate quasi! Dicta, nulla dolores reiciendis id delectus ab! Sunt soluta molestias animi fuga dicta minus, quia aperiam quis, porro vero molestiae quidem, suscipit voluptatibus cupiditate. Beatae hic neque excepturi, praesentium similique obcaecati repudiandae fugit quisquam temporibus, dolor dignissimos quae earum ab veritatis fuga maiores pariatur repellendus nemo? Iusto sequi fugiat adipisci recusandae. Doloribus porro est illum sapiente, blanditiis harum ullam corporis quidem repudiandae, eos laboriosam officiis nesciunt excepturi nam quaerat eveniet accusamus similique qui nostrum illo cum accusantium? Iusto?
         </div>
         <div className='result-preview'>
-
+            <LiveResult/>
+            <LiveResult/>
         </div>
     </div>
   )
